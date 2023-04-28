9e20f543afbb1412e8984915c29696297fbba370
Joe Hale <joe@jjhale.com>
2023-04-28 18:47:15 -0400

    remove env0 test

--- tests/env/test_clue_environment.py
+++ tests/env/test_clue_environment.py
@@ -2,17 +2,7 @@ from typing import cast
 
 from pettingzoo.test import api_test
 
-from clue.env import clue_environment_v0, clue_environment_v1, clue_environment_v2
-
-
-def test_api_test_v0() -> None:
-    env = clue_environment_v0.ClueEnvironment(max_players=6)
-    try:
-        api_test(env, num_cycles=1000, verbose_progress=False)
-    except Exception as e:
-        with open("test_output.txt", "w") as f:
-            f.write(cast(str, env.render()))
-        raise e
+from clue.env import clue_environment_v1, clue_environment_v2
 
 
 def test_api_test_v1() -> None:
