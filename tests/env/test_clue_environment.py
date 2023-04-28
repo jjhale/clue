7e7d338a87fb4b92b476f5622857ee0ab37d7e51
Joe Hale <joe@jjhale.com>
2023-04-28 17:53:05 -0400

    working interactive mode

--- tests/env/test_clue_environment.py
+++ tests/env/test_clue_environment.py
@@ -2,7 +2,7 @@ from typing import cast
 
 from pettingzoo.test import api_test
 
-from clue.env import clue_environment_v0, clue_environment_v1
+from clue.env import clue_environment_v0, clue_environment_v1, clue_environment_v2
 
 
 def test_api_test_v0() -> None:
@@ -17,6 +17,17 @@ def test_api_test_v0() -> None:
 
 def test_api_test_v1() -> None:
     env = clue_environment_v1.ClueEnvironment(max_players=6)
+
+    try:
+        api_test(env, num_cycles=1000, verbose_progress=False)
+    except Exception as e:
+        with open("test_output.txt", "w") as f:
+            f.write(cast(str, env.render()))
+        raise e
+
+
+def test_api_test_v2() -> None:
+    env = clue_environment_v2.ClueEnvironment(max_players=6)
 
     try:
         api_test(env, num_cycles=1000, verbose_progress=False)
--- tests/test_map.py
+++ tests/test_map.py
@@ -17,3 +17,16 @@ def test_move_towards_room(map_csv_location: str) -> None:
     board = Board(map_csv_location)
     assert board is not None
     board.move_towards_room(player_idx=0, throw=6, room_idx=2)
+
+
+def test_room_distances(map_csv_location: str) -> None:
+    board = Board(map_csv_location)
+
+    # put everyone in room r
+    for r in range(9):
+        for i in range(6):
+            board.move_to_room(player_idx=i, room_idx=r)
+            distances = board.distances[board.player_positions, :]
+
+            assert distances[i, r] == 0
+            assert distances[i, r + 9] == 0
